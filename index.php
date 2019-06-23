78722860781448ee653803ddb60298d2ba252df2
gdochadipa <31502864+gdochadipa@users.noreply.github.com>
2019-06-23 17:25:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,99 +1,89 @@
-<?php
-include 'function/main.php';
-$main = new main();
-if (isset($_GET['data'])) {
-  $get = $_GET['data'];
-}else {
-  $get = 'index';
-}
-
- ?>
-
-<!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="asset/style.css" media="screen" title="no title" charset="utf-8">
-  </head>
-  <body>
-    <?php
-    $query = "SELECT * from tbl_surat_masuk";
-    $execute = $main->execute($query);
-     ?>
-    <div class="" id="kontent" style="min-height: 100vh;">
-    <h1 class="page-title">Data Surat Masuk</h1>
-    <div class="menu-bar">
-      <div class="row">
-      <div class="col-6">
-            <a type="button"  class="btn medium bg-yellow" href="add_sm.php" > Tambah Surat Masuk</a>
-      </div>
-      <div class="col-6" style="text-align: right;">
-      <form class="form-input" action="" method="post">
-        <div class="row">
-          <div style="col-5">
-            <input type="text" id="search" placeholder="Cari Berdasarkan Nomor Surat" name="get_cari" value="">
-          </div>
-          <div style="col-5">
-            <input type="submit" class="btn medium bg-green" name="cari" value="Cari">
-          </div>
-        </div>
-    </div>
-      </div>
-      </form>
-    </div>
-      <div class="row">
-        <div style="width:100%;">
-          <div class="bordered-box yellow" >
+ <head>
+ <Title>Registration Form</Title>
+ <style type="text/css">
+ 	body { background-color: #fff; border-top: solid 10px #000;
+ 	    color: #333; font-size: .85em; margin: 20; padding: 20;
+ 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+ 	}
+ 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+ 	h1 { font-size: 2em; }
+ 	h2 { font-size: 1.75em; }
+ 	h3 { font-size: 1.2em; }
+ 	table { margin-top: 0.75em; }
+ 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+ 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+ </style>
+ </head>
+ <body>
+ <h1>Register here!</h1>
+ <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+ <form method="post" action="index.php" enctype="multipart/form-data" >
+       Name  <input type="text" name="name" id="name"/></br></br>
+       Email <input type="text" name="email" id="email"/></br></br>
+       Job <input type="text" name="job" id="job"/></br></br>
+       <input type="submit" name="submit" value="Submit" />
+       <input type="submit" name="load_data" value="Load Data" />
+ </form>
+ <?php
+    $host = "<Nama server database Anda>";
+    $user = "<Nama admin database Anda>";
+    $pass = "<Password admin database Anda>";
+    $db = "<Nama database Anda>";
 
-            <legend>Data Surat</legend>
-            <table>
-              <thead>
-                <th>ID</th>
-                <th>No Surat</th>
-                <th>Asal Surat</th>
-                <th>Subjek</th>
-                <th>Tgl Surat</th>
-                <th>Disposisi</th>
-                <th>Action</th>
-              </thead>
-              <tbody id="table" >
-                <?php while ($row = $execute->fetch_assoc()) { ?>
+    try {
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
 
-                <tr>
-                  <td><?php echo $row['id_surat']; ?></td>
-                  <td><?php echo $row['no_surat']; ?></td>
-                  <td><?php echo $row['asal_surat']; ?></td>
-                  <td><?php echo $row['subjek']; ?></td>
-                  <td><?php echo $row['tgl_surat']; ?></td>
-                  
-                     <?php  } ?>
-                 </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-
-
-
-    </div>
-    <script type="text/javascript">
-      document.getElementById('search').onkeyup = function() {
-        var filter,table,tr,i;
-        filter = document.getElementById('search').value.toLowerCase();
-        table = document.getElementById('table');
-        tr = table.getElementsByTagName('tr');
-        for (var i = 0; i < tr.length; i++) {
-          if (tr[i].innerHTML.toLowerCase().indexOf(filter) > -1) {
-            tr[i].style.display ="";
-          }else {
-            tr[i].style.display = "none";
-          }
+    if (isset($_POST['submit'])) {
+        try {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $job = $_POST['job'];
+            $date = date("Y-m-d");
+            // Insert data
+            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
+                        VALUES (?,?,?,?)";
+            $stmt = $conn->prepare($sql_insert);
+            $stmt->bindValue(1, $name);
+            $stmt->bindValue(2, $email);
+            $stmt->bindValue(3, $job);
+            $stmt->bindValue(4, $date);
+            $stmt->execute();
+        } catch(Exception $e) {
+            echo "Failed: " . $e;
         }
-      };
-    </script>
 
-  </body>
-</html>
-<?php ?>
+        echo "<h3>Your're registered!</h3>";
+    } else if (isset($_POST['load_data'])) {
+        try {
+            $sql_select = "SELECT * FROM Registration";
+            $stmt = $conn->query($sql_select);
+            $registrants = $stmt->fetchAll(); 
+            if(count($registrants) > 0) {
+                echo "<h2>People who are registered:</h2>";
+                echo "<table>";
+                echo "<tr><th>Name</th>";
+                echo "<th>Email</th>";
+                echo "<th>Job</th>";
+                echo "<th>Date</th></tr>";
+                foreach($registrants as $registrant) {
+                    echo "<tr><td>".$registrant['name']."</td>";
+                    echo "<td>".$registrant['email']."</td>";
+                    echo "<td>".$registrant['job']."</td>";
+                    echo "<td>".$registrant['date']."</td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "<h3>No one is currently registered.</h3>";
+            }
+        } catch(Exception $e) {
+            echo "Failed: " . $e;
+        }
+    }
+ ?>
+ </body>
+ </html>
